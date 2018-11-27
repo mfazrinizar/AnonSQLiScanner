120e1f913b2134903c64fc634f19e5bf9ce9f28c
Anon6372098 <anon6372098.id@gmail.com>
2018-11-27 15:24:14 +0700

    Start File

---
+++ AnonStart.sh
@@ -0,0 +1,64 @@
+#!/system/xbin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f slant -F gay "Anon Admin" | lolcat
+toilet -f slant -F gay "Finder" | lolcat
+echo "\033[34;1m"
+cal
+date
+echo ""      
+echo "\033[32;1mCreator : Anon6372098" 
+echo "\033[35;1mTeam : D4RK SYST3M F41LUR3 S33K3R"
+echo "\033[36;1mThanks to : All Member of DSFS"
+echo "\033[37;1mAlly Team : DSFS Public"
+echo "\033[33;1mContact Me : anon6372098.id@gmail.com"
+echo "\033[31;1mSubscribe Youtube : D4RK SYST3M F41LUR3 S33K3R" "\033[36;1m(DSFS)"
+echo "\033[34;1m[============================]"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Opsi :"
+echo "\033[32;1m"
+echo "[===============================================]"
+echo "A.> Anon SQLi Scanner (Scan Website Vuln SQLi)"
+echo "[===============================================]"
+echo "Q.> Exit/Keluar"
+echo "[===============================================]"
+read -p "Anon SQLi Scanner]~#" anon
+
+if [ $anon = A ] || [ $anon = a ]
+then
+clear
+figlet "Anon SQLi Scanner" | lolcat
+echo "\033[32;1m"
+cd Anon
+python2 Anon.py
+python2 Anon.py -petunjuk
+fi
+
+if [ $anon = Q ] || [ $anon = q ]
+then
+echo "\033[32;1mWe Are D4RK SYST3M F41LUR3 S33K3R (DSFS)"
+sleep 1
+echo "\033[32;1mWe Are DSFS Public"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo "We Are Legion"
+sleep 1
+echo "We Do Not Forgive"
+sleep 1
+echo "We Do Not Forget"
+sleep 1
+echo "Expect Us"
+sleep 1
+echo "\033[32;1mDiakui, Bukan Mengakui"
+sleep 1
+exit
+fi
